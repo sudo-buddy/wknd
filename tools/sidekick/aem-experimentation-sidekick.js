af495fba2c1b31a7ea4d8318f33380ef31b74aaa
Xinyi Feng <xfeng@adobe.com>
2025-01-08 15:11:43 -0800

    add sidekick.js

---
+++ tools/sidekick/aem-experimentation-sidekick.js
@@ -0,0 +1,32 @@
+(function () {
+  let isAEMExperimentationAppLoaded = false;
+  function loadAEMExperimentationApp() {
+    const script = document.createElement('script');
+    script.src = 'https://experience-qa.adobe.com/solutions/ExpSuccess-aem-experimentation-mfe/static-assets/resources/sidekick/client.js';
+    script.onload = function () {
+      isAEMExperimentationAppLoaded = true;
+    };
+    script.onerror = function () {
+      console.error('Error loading AEMExperimentationApp.');
+    };
+    document.head.appendChild(script);
+  }
+
+  function handlePluginButtonClick() {
+    if (!isAEMExperimentationAppLoaded) {
+      loadAEMExperimentationApp();
+    }
+  }
+
+  const sidekick = document.querySelector('helix-sidekick');
+  if (sidekick) {
+    // sidekick already loaded
+    sidekick.addEventListener('custom:aem-experimentation-sidekick', handlePluginButtonClick);
+  } else {
+    // wait for sidekick to be loaded
+    document.addEventListener('sidekick-ready', () => {
+      document.querySelector('helix-sidekick')
+        .addEventListener('custom:aem-experimentation-sidekick', handlePluginButtonClick);
+    }, { once: true });
+  }
+}());
