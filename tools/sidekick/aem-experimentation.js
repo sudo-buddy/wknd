04993d2f7594d8d65e01b193fefc316420b9c647
Xinyi Feng <xfeng@adobe.com>
2025-01-09 08:51:08 -0800

    improve aem-experimentation code

--- tools/sidekick/aem-experimentation.js
+++ tools/sidekick/aem-experimentation.js
@@ -1,33 +1,40 @@
 (function () {
   let isAEMExperimentationAppLoaded = false;
+  
   function loadAEMExperimentationApp() {
-    const script = document.createElement('script');
-    script.src = 'https://experience-qa.adobe.com/solutions/ExpSuccess-aem-experimentation-mfe/static-assets/resources/sidekick/client.js?source=plugin&ExpSuccess-aem-experimentation-mfe_version=PR-33-f7ed54bb4da0f60763777a02f54000c89e2c6828';
-    script.onload = function () {
-      isAEMExperimentationAppLoaded = true;
-    };
-    script.onerror = function () {
-      console.error('Error loading AEMExperimentationApp.');
-    };
-    document.head.appendChild(script);
+      const script = document.createElement('script');
+      script.src = 'https://experience-qa.adobe.com/solutions/ExpSuccess-aem-experimentation-mfe/static-assets/resources/sidekick/client.js?source=plugin&ExpSuccess-aem-experimentation-mfe_version=PR-33-f7ed54bb4da0f60763777a02f54000c89e2c6828';
+      script.onload = function () {
+          isAEMExperimentationAppLoaded = true;
+          // Trigger initial show
+          const event = new CustomEvent('custom:aem-experimentation-sidekick');
+          document.querySelector('aem-sidekick').dispatchEvent(event);
+      };
+      script.onerror = function () {
+          console.error('Error loading AEMExperimentationApp.');
+          isAEMExperimentationAppLoaded = false;
+      };
+      document.head.appendChild(script);
   }
 
   function handlePluginButtonClick() {
-    console.log('handlePluginButtonClick');
-    if (!isAEMExperimentationAppLoaded) {
-      loadAEMExperimentationApp();
-    }
+      console.log('handlePluginButtonClick');
+      if (!isAEMExperimentationAppLoaded) {
+          loadAEMExperimentationApp();
+      } else {
+          // For subsequent clicks, dispatch event to toggle visibility
+          const event = new CustomEvent('custom:aem-experimentation-sidekick');
+          document.querySelector('aem-sidekick').dispatchEvent(event);
+      }
   }
 
   const sidekick = document.querySelector('aem-sidekick');
   if (sidekick) {
-    // sidekick already loaded
-    sidekick.addEventListener('custom:aem-experimentation-sidekick', handlePluginButtonClick);
+      sidekick.addEventListener('custom:aem-experimentation-sidekick', handlePluginButtonClick);
   } else {
-    // wait for sidekick to be loaded
-    document.addEventListener('sidekick-ready', () => {
-      document.querySelector('aem-sidekick')
-        .addEventListener('custom:aem-experimentation-sidekick', handlePluginButtonClick);
-    }, { once: true });
+      document.addEventListener('sidekick-ready', () => {
+          document.querySelector('aem-sidekick')
+              .addEventListener('custom:aem-experimentation-sidekick', handlePluginButtonClick);
+      }, { once: true });
   }
 }());
