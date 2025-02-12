463360b87207d460deed38df98d8f763128d9c0c
Xinyi Feng <xfeng@adobe.com>
2025-02-12 08:36:23 -0800

    test

--- tools/sidekick/aem-experimentation.js
+++ tools/sidekick/aem-experimentation.js
@@ -26,7 +26,7 @@
           }
 
           const script = document.createElement('script');
-          script.src = 'https://experience-qa.adobe.com/solutions/ExpSuccess-aem-experimentation-mfe/static-assets/resources/sidekick/client.js?source=plugin';
+          script.src = 'https://experience-qa.adobe.com/solutions/ExpSuccess-aem-experimentation-mfe/static-assets/resources/sidekick/client.js?source=plugin&ExpSuccess-aem-experimentation-mfe_version=PR-100-f3b078c1332015929140bca1249d24e3295038e6';
 
           script.onload = function () {
               isAEMExperimentationAppLoaded = true;
